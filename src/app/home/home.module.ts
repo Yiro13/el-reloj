f8852d978d68508c82b452247b84ea6ef90f8f19
Yahir Leo <yahir.banuelos5763@alumnos.udg.mx>
2023-04-25 15:32:06 -0600

    home_page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './pages/home/home.component';
 import { SharedModule } from '../shared/shared.module';
+import { InfoComponent } from './component/info/info.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
+    InfoComponent
   ],
   imports: [
     CommonModule,
