0b94bd4dab6e4a8d1ad62a932ad1dbe73e418b68
Yahir Leo <yahirrocki@outlook.com>
2023-04-24 21:58:35 -0600

    routing and modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeModule } from './home/home.module';
+import { ProductsModule } from './products/products.module';
+import { ContactModule } from './contact/contact.module';
+import { NotFoundModule } from './not-found/not-found.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+  {path: 'home', loadChildren: () => HomeModule},
+  {path: 'productos', loadChildren: () => ProductsModule},
+  {path: 'contacto', loadChildren: () => ContactModule},
+  {path: '**', loadChildren: () => NotFoundModule}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
